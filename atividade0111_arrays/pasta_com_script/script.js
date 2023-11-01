fd64ca484b4d0455e6e43a31dd422645ada39f31
Gian Lucas <glrancan@hotmail.com>
2023-11-01 17:39:17 -0300

    nova atividade 01/11 - arrays

---
+++ atividade0111_arrays/pasta_com_script/script.js
@@ -0,0 +1,59 @@
+ // 1. Criar um array e percorrê-lo utilizando o loop for
+
+        // a. Crie um array com o nome "nomes" e adicione 4 nomes de
+        // pessoas que você conhece
+
+        // b. Utilize o loop for para percorrer o array e exibir os nomes na
+        // tela
+
+        // let nomes = ["Douglas", "Luna", "Marcelo", "Sandro"]
+
+        // for (let nome  of nomes) {
+        //     document.write(`${nome} <br>`)
+        // }
+
+// 2. Criar um array e percorrê-lo utilizando o loop for of
+
+        // a. Crie um array com o nome "numeros" e adicione 5 números de
+        // sua escolha
+
+        // b. Utilize o loop for of para percorrer o array e exibir a soma dos
+        // números na tela
+
+        // let numeros = [7, 19, 24, 31]
+        // let soma = 0
+
+        // for (let numero of numeros) {
+        //     soma += numero
+        // } 
+        // document.write(`A soma dos números é: ${soma}`)
+
+// <!-- 3. Utilizar os métodos push, pop, unshift e shift para manipular um
+        // array
+    
+        // a. Crie um array com o nome "frutas" e adicione 3 tipos de frutas
+        // de sua escolha
+            
+        // b. Utilize o método push para adicionar uma fruta no final do
+        // array
+    
+        // c. Utilize o método pop para remover a última fruta do array
+    
+        // d. Utilize o método unshift para adicionar uma fruta no início do
+        // array
+    
+        // e. Utilize o método shift para remover a primeira fruta do array
+    
+        // f. Exiba o array resultante na tela -->
+
+        // let frutas = ['Maça', 'Bergamota', 'Kiwi']
+
+        // frutas.push('Ameixa')
+
+        // frutas.pop()
+
+        // frutas.unshift('Banana')
+
+        // frutas.shift()
+
+        // document.write(frutas)
